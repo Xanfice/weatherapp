1913ba794ecbb7045532b3635b00abeeaf88c9ee
Xanfice <moozhan.poodi@gmail.com>
2023-01-03 23:21:12 +0330

    code2

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -1,28 +1,29 @@
-function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
+let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+
+let hour = date.getHours();
+  if (hours < 10) {
+    hours = `0${hour}`;
+  }
   function displayWeatherCondition(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(
@@ -84,4 +85,3 @@ function formatDate(date) {
   currentLocationButton.addEventListener("click", getCurrentLocation);
   
   searchCity("New York");
-  
